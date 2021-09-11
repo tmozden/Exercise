79ff5f2c7b2f422f9f80cf4d9229de6631b0bdc1
Travis M <1341708+tmozden@users.noreply.github.com>
2021-09-11 07:19:27 -0500

    Initial Commit

---
+++ ExerciseApp/ExerciseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseAppApp.swift
+//  ExerciseApp
+//
+//  Created by Travis Mozden on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
